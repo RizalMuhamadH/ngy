719e334c6082b7de40e9848350a8bf8161e33dc3
Rizal_Muhamad_H <rizalmuhamadh@gmail.com>
2019-04-06 21:50:49 +0700

    error handling

--- application/controllers/Customer.php
+++ application/controllers/Customer.php
@@ -57,6 +57,25 @@ class Customer extends CI_Controller
                 }
             }
 
+            
+            $config['cacheable']    = true; //boolean, the default is true
+            $config['cachedir']     = '/assets/'; //string, the default is application/cache/
+            $config['errorlog']     = '/assets/'; //string, the default is application/logs/
+            $config['imagedir']     = '/assets/images/'; //direktori penyimpanan qr code
+            $config['quality']      = true; //boolean, the default is true
+            $config['size']         = '1024'; //interger, the default is 1024
+            $config['black']        = array(224,255,255); // array, default is array(255,255,255)
+            $config['white']        = array(70,130,180); // array, default is array(0,0,0)
+            $this->ciqrcode->initialize($config);
+
+            $image_name=$row->t_no_trans.'.png'; //buat name dari qr code sesuai dengan nim
+    
+            $params['data'] = $row->t_no_trans; //data yang akan di jadikan QR CODE
+            $params['level'] = 'H'; //H=High
+            $params['size'] = 10;
+            $params['savename'] = FCPATH.$config['imagedir'].$image_name; //simpan image QR CODE ke folder assets/images/
+            $this->ciqrcode->generate($params); // fungsi untuk generate QR CODE
+
 
             $data = array(
 		'c_id' => $row->c_id,
@@ -184,23 +203,6 @@ class Customer extends CI_Controller
                 'c_id ' => $idCustomer,
             );
 
-            $config['cacheable']    = true; //boolean, the default is true
-            $config['cachedir']     = '/assets/'; //string, the default is application/cache/
-            $config['errorlog']     = '/assets/'; //string, the default is application/logs/
-            $config['imagedir']     = '/assets/images/'; //direktori penyimpanan qr code
-            $config['quality']      = true; //boolean, the default is true
-            $config['size']         = '1024'; //interger, the default is 1024
-            $config['black']        = array(224,255,255); // array, default is array(255,255,255)
-            $config['white']        = array(70,130,180); // array, default is array(0,0,0)
-            $this->ciqrcode->initialize($config);
-
-            $image_name=$noTrans.'.png'; //buat name dari qr code sesuai dengan nim
-    
-            $params['data'] = $noTrans; //data yang akan di jadikan QR CODE
-            $params['level'] = 'H'; //H=High
-            $params['size'] = 10;
-            $params['savename'] = FCPATH.$config['imagedir'].$image_name; //simpan image QR CODE ke folder assets/images/
-            $this->ciqrcode->generate($params); // fungsi untuk generate QR CODE
 
             $this->Transaction_model->insert($trans);
 
---
+++ application/logs/log-2019-04-06.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-04-06 13:36:52 --> Severity: Notice --> Undefined property: stdClass::$_privileges C:\xampp\htdocs\ngy\application\models\User_model.php 100
+ERROR - 2019-04-06 13:36:53 --> 404 Page Not Found: Assets/AdminLTE-2.4.5
+ERROR - 2019-04-06 13:37:00 --> 404 Page Not Found: Assets/AdminLTE-2.4.5
+ERROR - 2019-04-06 13:37:19 --> 404 Page Not Found: Assets/AdminLTE-2.4.5
+ERROR - 2019-04-06 14:03:54 --> 404 Page Not Found: Assets/AdminLTE-2.4.5
+ERROR - 2019-04-06 14:34:26 --> 404 Page Not Found: Assets/AdminLTE-2.4.5
+ERROR - 2019-04-06 14:34:30 --> 404 Page Not Found: Assets/AdminLTE-2.4.5
