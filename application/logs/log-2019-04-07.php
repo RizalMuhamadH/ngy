c44b64a625e8618df8a019e26f96baf0ce8c76f2
Rizal_Muhamad_H <rizalmuhamadh@gmail.com>
2019-04-07 20:02:00 +0700

    log

---
+++ application/logs/log-2019-04-07.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-04-07 12:47:23 --> Severity: Notice --> Undefined property: stdClass::$_privileges C:\xampp\htdocs\ngy\application\models\User_model.php 100
+ERROR - 2019-04-07 12:47:23 --> 404 Page Not Found: Assets/AdminLTE-2.4.5
+ERROR - 2019-04-07 12:47:27 --> 404 Page Not Found: Assets/AdminLTE-2.4.5
